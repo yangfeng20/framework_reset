98f0da83cf5744f34cb3d32b161f8b08ce460a88
yangfeng20 <2478341687@qq.com>
2022-10-12 14:42:37 +0800

    feat(mapper):修复mapper跳转问题

---
+++ mybatis/src/main/java/com/maple/mybatis/quick/StudentMapper.java
@@ -0,0 +1,16 @@
+package com.maple.mybatis.quick;
+
+/**
+ * @author 杨锋
+ * @date 2022/10/12 10:01
+ * desc:
+ */
+
+public interface StudentMapper {
+
+
+    /**
+     * 插入学生数据
+     */
+    void insertStudent();
+}
