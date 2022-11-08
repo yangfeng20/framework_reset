f784c69f4d207a43ea863523bb9dab9cf986e900
yangfeng20 <2478341687@qq.com>
2022-11-08 21:11:19 +0800

    feat(rabbit):优先级队列

--- rabbitmq/mq-springboot/src/main/java/com/maple/mqspringboot/MessageSenderController.java
+++ rabbitmq/mq-springboot/src/main/java/com/maple/mqspringboot/MessageSenderController.java
@@ -2,12 +2,15 @@ package com.maple.mqspringboot;
 
 import com.maple.mqspringboot.config.RabbitMqConst;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -30,6 +33,25 @@ public class MessageSenderController {
 
         String dateStr = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
         amqpTemplate.convertAndSend(RabbitMqConst.EXCHANGE, "dddddd", msg + " " + dateStr);
+        return dateStr;
+    }
+
+
+    /**
+     * 发送消息优先级
+     *
+     * @param msg 味精
+     * @return {@link String}
+     */
+    @RequestMapping(value = "/send/message/{msg}/{priority}", method = RequestMethod.GET)
+    public String senderMessagePriority(@PathVariable String msg, @PathVariable Integer priority) {
+
+        String dateStr = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        Message message = MessageBuilder
+                .withBody((msg + " " + dateStr).getBytes(StandardCharsets.UTF_8))
+                .setPriority(priority)
+                .build();
+        amqpTemplate.convertAndSend(RabbitMqConst.EXCHANGE, RabbitMqConst.KEY, message);
         return dateStr;
     }
 }
--- rabbitmq/mq-springboot/src/main/java/com/maple/mqspringboot/config/MqQueueConfig.java
+++ rabbitmq/mq-springboot/src/main/java/com/maple/mqspringboot/config/MqQueueConfig.java
@@ -1,6 +1,11 @@
 package com.maple.mqspringboot.config;
 
+import com.rabbitmq.client.AMQP;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
 
 /**
  * @author 杨锋
@@ -11,6 +16,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MqQueueConfig {
 
+
+    @Bean
+    public Queue priorityQueue(){
+        // 同时发送消息是也需要设置properties
+        AMQP.BasicProperties properties = new AMQP.BasicProperties().builder().priority(5).build();
+
+
+        // x-max-priority参数的值应该介于1到255。建议使用1到10之间的队列。
+        HashMap<String, Object> map = new HashMap<>();
+        // 队列开启优先级，最大10，可设置255
+        map.put("x-max-priority", 10);
+        return new Queue("priorityQueue", false, false, true, map);
+    }
 
 
 }
