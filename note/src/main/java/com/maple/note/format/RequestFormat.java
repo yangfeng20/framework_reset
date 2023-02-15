68788cce70101a1c41145b8e9d5a2045c93ec855
yangfeng20 <2478341687@qq.com>
2023-02-15 20:15:52 +0800

    feat(service):requestFormat

--- note/src/main/java/com/maple/note/format/RequestFormat.java
+++ note/src/main/java/com/maple/note/format/RequestFormat.java
@@ -9,10 +9,13 @@ package com.maple.note.format;
 public class RequestFormat {
 
 
-    private static final String modelValue = "{\"formValues\":{\"attachmentList\":[],\"cargoJson\":{\"cargoType\":1},\"cargoTypeExtend\":\"{\\\"CargoTypeID\\\":1,\\\"CargoTypeName\\\":\\\"普货\\\",\\\"Children\\\":[]}\",\"consigneeJson\":{\"address\":\"\",\"customerReferenceNo\":\"123\",\"name\":\"\"},\"counterTerrorismJson\":{},\"docId\":1575894,\"documentParam\":{\"isVirtualChannel\":false,\"tags\":[3002]},\"isOpenCheck\":true,\"isSaveRuleVerificationDesc\":1,\"isSyncVgm\":false,\"lbContainerJson\":[],\"lineCarrierId\":15690,\"modifyProperties\":[\"bookingNo\",\"cargoJson\",\"cargoJson.cargoType.label\",\"counterTerrorismJson.cargoOfLoadingNo\",\"documentParam.cargoOwner\",\"cargoTypeExtend\",\"lineCarrierId\",\"consigneeJson\",\"containerCargoJson\",\"documentParam.customerBpId\",\"documentParam.customerBpName\",\"docId\",\"documentParam.docRoleId\",\"counterTerrorismJson.eightThousandCode\",\"formChanged\",\"groupId\",\"counterTerrorismJson.isNeedLoadOfbill\",\"isUploadFile\",\"lbComment\",\"lbContainerJson\",\"lbCopyOriginalNo\",\"lbOriginalNo\",\"lbOtherComment\",\"lbShowInfo\",\"lbVisaDate\",\"lbVisaTerminalId\",\"lineCustomsVoyage\",\"lineDischargingTerminalId\",\"lineEstimateArrPortDate\",\"lineEstimateDeliveryDate\",\"lineFromTerminalId\",\"lineReceiptPlaceTerminalId\",\"lineToTerminalId\",\"lineLbVessel\",\"mblno\",\"mdDocId\",\"nameAccountJson\",\"needConfirmUpdated\",\"notifyJson\",\"documentParam.operationId\",\"documentParam.externalOrderNo\",\"payerJson\",\"documentParam.relatedUserId\",\"documentParam.remark\",\"documentParam.saleId\",\"counterTerrorismJson.scacCode\",\"secondNotifyJson\",\"sendStatusArea\",\"documentParam.serviceId\",\"shipContractNo\",\"shipDeliveryModeId\",\"shipPaymentTerminalId\",\"shipPaymentWayId\",\"shipperJson\",\"shipShippingClauseId\",\"documentParam.tags\",\"transportInfo\",\"lineLbVoyage\",\"tags\",\"cargoJson\",\"documentParam\",\"rbSendChannelVO\",\"containerCargoJson\",\"containerJson\",\"mBlno\",\"lineFromTerminalId\",\"lineFromTerminal\",\"lineReceiptPlaceTerminalId\",\"lineReceiptPlaceTerminal\",\"lineDischargingTerminalId\",\"lineDischargingTerminal\",\"lineToTerminalId\",\"lineToTerminal\",\"counterTerrorismJson\",\"cargoTypeExtend\",\"taskId\",\"shipmentReferenceJson\",\"docChannel\",\"containerMapping\",\"packageMapping\",\"lineReceiptPlaceTerminalMapping\",\"lineFromTerminalMapping\",\"lineDischargingTerminalMapping\",\"lineToTerminalMapping\",\"configGroupId\",\"docChannelList\",\"configGroupIdList\",\"closingCustomDate\",\"billCloseDate\",\"vgmCloseDate\",\"openPortDate\",\"closePortDate\"],\"nameAccountJson\":{\"address\":\"\",\"aeoCode\":\"123\",\"cityName\":\"123\",\"countryCode\":\"CN\",\"countryId\":704,\"countryName\":\"CHINA\",\"customerReferenceNo\":\"\",\"provinceName\":\"\",\"zipCode\":\"1233\"},\"notifyJson\":{\"provinceName\":\"123\"},\"payerJson\":{\"code\":\"\",\"customerReferenceNo\":\"付款方PO\",\"exportRegistrarCode\":\"123\",\"importExportCode\":\"\",\"nvoccCertificateNo\":\"付款方NVOCC\",\"zipCode\":\"123\"},\"rbSendChannelVO\":{\"docId\":1575894,\"sendChannelVO\":{\"operationType\":1}},\"realConsignee\":{},\"realNotify\":{},\"realSecondNotify\":{},\"realShipper\":{\"provinceName\":\"123\"},\"secondNotifyJson\":{\"customerReferenceNo\":\"第二通知人PO\",\"nvoccCertificateNo\":\"第二通知人NVOCC\"},\"shipperJson\":{}},\"isAutoCheck\":true,\"validateFields\":[]}";
+    private static final
+
+    String MODEL_VALUE = "{\"page\":1,\"size\":20,\"sort\":[],\"taskType\":211}";
 
 
     public static void main(String[] args) {
+        // 现在的model是江苏远洋租户，并且为江苏远洋管理员
         String str = "accountId:1\n" +
                 "accountTenantId:3453540\n" +
                 "ext:\n" +
@@ -26,7 +29,6 @@ public class RequestFormat {
                 "xReadTimeoutMillis:0\n" +
                 "xSourceAppId:\"61503\"\n" +
                 "xTenantId:3454329\n" +
-                "xTraceId:\"335471ff295449a0a403d70eb7216d7a.111.16738376910898521\"\n" +
                 "xUserId:3469467\n" +
                 "xUserName:\"江苏远洋管理\"";
 
@@ -77,10 +79,11 @@ public class RequestFormat {
         strBuilder.append("}");
 
         String headerValue = strBuilder.toString();
+        System.out.println("header数据：");
         System.out.println(headerValue);
 
-
-        String request = "{\"header\":"+headerValue +",\"model\":"+modelValue+"}";
+        System.out.println("\nrequest数据：");
+        String request = "{\"header\":"+headerValue +",\"model\":"+MODEL_VALUE+"}";
         System.out.println(request);
 
     }
